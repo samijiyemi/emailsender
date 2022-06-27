8c39f8e2d5e8cf306f6796aef4045f87846c5382
ogbenisamu <samijiyemi@gmail.com>
2022-06-27 14:31:35 +0100

    import module

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("hello world")
+from email.mime.multipart import MIMEMultipart
+
+#
+message = MIMEMultipart()
+
+# E-mail Body
+message['to'] = "samijiyemi2gmail.com"
+message['from'] = "info@samijiyemi.co.uk"
+message['subject'] = "Sending E-mail From Python"
