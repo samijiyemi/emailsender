f6b5a18a307b7e6a2c51c2c3d2db6a96abbd488c
ogbenisamu <samijiyemi@gmail.com>
2022-06-28 08:29:23 +0100

    minor changes

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-#
+password = input("what is your password: ")
+print(password)
+
 message = MIMEMultipart()
 
-# E-mail Body
-message['to'] = "samijiyemi2gmail.com"
-message['from'] = "info@samijiyemi.co.uk"
-message['subject'] = "Sending E-mail From Python"
+message['from'] = "Sam Ijiyemi"
+message['to'] = "sam@eduboxglobal.org"
+message['subject'] = "Congratulations!!!"
+message.attach(MIMEText("Body Message from Sam"))
+
+with smtplib.SMTP_SSL("smtp.gmail.com") as server:
+    server.ehlo()
+    server.starttls()
+    server.login("sam@eduboxglobal.org", password=password)
+    server.send_message(message)
+    print("E-mail Sent!")
